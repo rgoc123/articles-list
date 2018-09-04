44099dbc27a2a3320a121319f6d40971170ce212
Robert O'Connor <robertgoconnor2@gmail.com>
2018-09-04 17:49:10 -0400

    Create button to load more articles

--- src/App.js
+++ src/App.js
@@ -4,15 +4,46 @@ import articles from './data/articles';
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      articlesList: articles.slice(0,9),
+      loadNumber: 1
+    };
+  }
+
+  // Function for adding more articles
+    // Reference state.loadNumber
+    // Append more rows
+    // If there aren't more rows in articles, make call to more articles
+    // If there aren't more in more articles, make button disappear
+
+  // Possible ways to do this:
+    // Append new elements to DOM
+      // Benefit: It might look cleaner
+      // Cost: It'll be more of a pain to code
+    // Have a state key "articles" and add to that
+      // Benefit: It'll be easier to code, and there's probably no
+      // difference in performance.
+  loadMoreArticles() {
+    let newLoadNumber = this.state.loadNumber + 1;
+    this.setState({loadNumber: newLoadNumber});
+  }
+
+
   // Creates the rows for each article
   // UPDATE: Probably want to pass in the data source as an argument
   // to make the function more resuseble.
-  createArticleRows() {
-    return articles.map(article => {
-      return (
-        <li className="article-li">
+  createArticleRows(loadNumber) {
+    let articlesList = [];
+    let end = loadNumber * 10;
+
+    for (let i = 0; i < end; i++) {
+      let article = articles[i];
+      articlesList.push(
+        <li className="article-li" key={i}>
           <div className="article-item">
-            <img src={article.image} />
+            <img src={article.image} alt={article.title} />
             <div className="article-info">
               <a href={article.url} target="_blank">{article.title}</a>
               <div>Shares: {article.shares}</div>
@@ -31,7 +62,9 @@ class App extends Component {
           </div>
         </li>
       );
-    });
+    }
+
+    return articlesList;
   }
 
   render() {
@@ -39,8 +72,9 @@ class App extends Component {
       <div className="App">
         <h1>Hello World!</h1>
         <ul>
-          {this.createArticleRows()}
+          {this.createArticleRows(this.state.loadNumber)}
         </ul>
+        <button onClick={() => this.loadMoreArticles()}>Load More</button>
       </div>
     );
   }
